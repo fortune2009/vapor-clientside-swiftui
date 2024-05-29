106e19ff4692b73f5ff586dba6fffbdf4a9e571e
Fortune David <fortunekbz2009@gmail.com>
2024-05-29 02:10:45 +0100

    Initial Commit

---
+++ YT-Vapor-iOS-App/YT_Vapor_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_Vapor_iOS_AppApp.swift
+//  YT-Vapor-iOS-App
+//
+//  Created by Fortune David Chigozirim on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct YT_Vapor_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
